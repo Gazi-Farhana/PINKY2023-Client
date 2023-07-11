ed0cdbaa525aa74f65b6dcbe9d6f784085ecfe89
Gazi-Farhana <gazifarhana95@gmail.com>
2023-07-11 15:04:23 +0600

    header name changed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -95,7 +95,7 @@ const Navbar = () => {
           </ul>
         </div>
         <Link href="/" className="btn btn-ghost normal-case text-xl">
-          elysianFeeds
+          Pinky
         </Link>
       </nav>
 
